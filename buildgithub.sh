356f8f7c2a006dd7474a1eb6b7358ff2590f7cec
Luis-Harz <luisharz@outlook.de>
2026-02-14 11:27:50 +0100

    Update: added gitignore

--- buildgithub.sh
+++ buildgithub.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 VERSION=$(cat build/Linux/version.txt)
+
 gh release create "$VERSION" \
-  build/Linux/SimpleApps build/Windows/SimpleApps.exe \
-  build/Linux.zip build/Windows.zip \
+  build/Linux/SimpleApps \
+  build/Windows/SimpleApps.exe \
+  build/Linux.zip \
+  build/Windows.zip \
   --title "SimpleApps v$VERSION" \
-  --notes "Release $VERSION: a new release of SimpleApps"
+  --notes "Release $VERSION: Includes Linux and Windows binaries and zips"
