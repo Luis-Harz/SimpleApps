5cfb94d20b0d9f47dd79ad3705da2b9c03002524
Luis-Harz <luisharz@outlook.de>
2026-02-24 17:49:00 +0100

    Updated install.bat

--- PermSetup/install.bat
+++ PermSetup/install.bat
@@ -3,18 +3,18 @@ echo ----SimpleApps Permanent----
 echo This installer will permanently install SimpleApps
 echo Press ENTER to start
 pause
+
 echo Making Directory...
-mkdir C:\SimpleApps /p
+mkdir "C:\SimpleApps" /p
+
 echo Copying Files...
-copy ..\* C:\SimpleApps
-echo Creating Desktop Icon
-powershell -Command ^
-"$WshShell = New-Object -ComObject WScript.Shell; ^
-$Shortcut = $WshShell.CreateShortcut('$env:USERPROFILE\Desktop\SimpleApps.lnk'); ^
-$Shortcut.TargetPath = 'C:\SimpleApps\SimpleLauncher.bat'; ^
-$Shortcut.WorkingDirectory = 'C:\SimpleApps'; ^
-$Shortcut.IconLocation = 'C:\SimpleApps\icon.ico'; ^
-$Shortcut.Save()"
-echo Adding to PATH
+xcopy "..\*" "C:\SimpleApps\" /s /e /y
+
+echo Creating Desktop Icon...
+powershell -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut(\"$env:USERPROFILE\Desktop\SimpleApps.lnk\"); $Shortcut.TargetPath = 'C:\SimpleApps\SimpleLauncher.bat'; $Shortcut.WorkingDirectory = 'C:\SimpleApps'; $Shortcut.IconLocation = 'C:\SimpleApps\icon.ico'; $Shortcut.Save()"
+
+echo Adding to PATH...
 setx PATH "%PATH%;C:\SimpleApps"
-echo "All Done!"
+
+echo All Done!
+pause
